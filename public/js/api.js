b982c2aca89198d6ec9d106aa20d8d404a554c3a
Williamsolamide <117740544+Williamsolamide@users.noreply.github.com>
2023-11-20 15:32:54 +0100

    first commit

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "a17c96050f5d70ab9657b7d911f715f0";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
